bdaa89473a1bb427cdc87deee4640856f23dc8fc
Pavel Okun <pavelokun17@gmail.com>
2023-06-30 18:35:57 +0200

    AppIcon updates

--- packages/components/src/AppIcon.tsx
+++ packages/components/src/AppIcon.tsx
@@ -1,20 +1,25 @@
 import { FunctionComponent } from 'react';
 import { ImageProps, Image } from 'react-native';
+import { SvgProps } from 'react-native-svg';
 
-type AppIconProps = ImageProps & {
-  source: number | FunctionComponent;
-};
+type AppIconProps<T extends number | FunctionComponent> = T extends number
+  ? { source: T; color?: string } & Omit<ImageProps, 'source'>
+  : { source: T } & SvgProps;
 
-export const AppIcon = (props: AppIconProps) => {
-  const { source, ...restProps } = props;
+export const AppIcon = <T extends number | FunctionComponent>(
+  props: AppIconProps<T>,
+) => {
+  const { source, color, ...restProps } = props;
 
   // If source is a number, it's an image resource
   if (typeof source === 'number') {
-    return <Image source={source} {...restProps} />;
+    return (
+      <Image source={source} {...(restProps as Omit<ImageProps, 'source'>)} />
+    );
   }
   // If not, it's a SVG component
   else {
-    const SvgComponent = source;
-    return <SvgComponent {...restProps} />;
+    const SvgComponent = source as FunctionComponent<SvgProps>;
+    return <SvgComponent color={color} {...restProps} />;
   }
 };
