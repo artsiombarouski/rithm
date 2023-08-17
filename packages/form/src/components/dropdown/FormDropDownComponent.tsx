b7f341e2fe9354fcba8bff88f433584e24e1150b
Pavel Okun <pavelokun17@gmail.com>
2023-08-17 08:55:17 +0300

    Fix FormDropDownComponent on safari

--- packages/form/src/components/dropdown/FormDropDownComponent.tsx
+++ packages/form/src/components/dropdown/FormDropDownComponent.tsx
@@ -1,4 +1,4 @@
-import { isIOSChrome, isIOSSafari } from '../../utils';
+import { isIOSChrome, isIOSSafari, isSafari } from '../../utils';
 import FormDropDownMenu from './FormDropDownMenu';
 import { useFocusRing } from '@react-native-aria/focus';
 import { useHover } from '@react-native-aria/interactions';
@@ -161,7 +161,9 @@ export function FormDropDownComponent(props: FormDropDownComponentProps) {
           InputRightElement={rightIcon}
           {...nonLayoutProps}
           selection={
-            isIOSSafari || isIOSChrome ? undefined : nonLayoutProps.selection
+            isIOSSafari || isIOSChrome || isSafari
+              ? undefined
+              : nonLayoutProps.selection
           } // https://github.com/GeekyAnts/NativeBase/issues/5111#issuecomment-1450106575
           // NOTE: Adding ts-ignore as we're not exposing isFocused in the Input component
           // @ts-ignore-next-line
--- packages/form/src/utils.tsx
+++ packages/form/src/utils.tsx
@@ -1,4 +1,4 @@
-import { TextInputProps } from 'react-native';
+import { Platform, TextInputProps } from 'react-native';
 
 export function useInputAutoHeight(
   props: Pick<TextInputProps, 'onChange' | 'onLayout' | 'multiline'> = {},
@@ -28,8 +28,17 @@ export function useInputAutoHeight(
   };
 }
 
-const ua = window.navigator.userAgent;
+const ua = window.navigator?.userAgent;
+export const isWeb = Platform.OS === 'web';
 const isIOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i);
 const isWebKit = !!ua.match(/WebKit/i);
 export const isIOSSafari = isIOS && isWebKit && !ua.match(/CriOS/i);
 export const isIOSChrome = isIOS && isWebKit && !ua.match(/Version/i);
+
+export const isSafari =
+  isWeb &&
+  navigator &&
+  navigator.vendor?.match(/apple/i) &&
+  !navigator.userAgent?.match(/crios/i) &&
+  !navigator.userAgent?.match(/fxios/i) &&
+  !navigator.userAgent?.match(/Opera|OPT\//);
