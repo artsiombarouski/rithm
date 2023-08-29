32889f9056757507a350209d5931f51d48a863da
Pavel Okun <pavelokun17@gmail.com>
2023-08-29 18:18:49 +0300

    Add containerProps to TextElementEdit

--- packages/page-builder/src/elements/text/TextElementEdit.tsx
+++ packages/page-builder/src/elements/text/TextElementEdit.tsx
@@ -1,24 +1,30 @@
 import { PageBuilderElementEditProps } from '../../types';
 import { FormInput, FormInputProps } from '@artsiombarouski/rn-form';
+import { Box, IBoxProps } from 'native-base';
 
 export type TextElementEditProps = PageBuilderElementEditProps &
-  Partial<FormInputProps>;
+  Partial<FormInputProps> & {
+    containerProps?: IBoxProps;
+  };
 
 export const TextElementEdit = (props: TextElementEditProps) => {
+  const { containerProps, ...restProps } = props;
   return (
-    <FormInput
-      {...props}
-      multiline={true}
-      keepErrorSpace={false}
-      borderStyle={'none'}
-      variant={'unstyled'}
-      p={3}
-      textAlignVertical={'top'}
-      borderColor={'transparent'}
-      placeholder={'Add some text here'}
-      _input={{
-        minHeight: '120px',
-      }}
-    />
+    <Box {...containerProps}>
+      <FormInput
+        multiline={true}
+        keepErrorSpace={false}
+        borderStyle={'none'}
+        variant={'unstyled'}
+        p={3}
+        textAlignVertical={'top'}
+        borderColor={'transparent'}
+        placeholder={'Add some text here'}
+        _input={{
+          minHeight: '120px',
+        }}
+        {...restProps}
+      />
+    </Box>
   );
 };
